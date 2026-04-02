f2a0e34a91f07ce4c5bc042168d4c5654bee36b2
RemadaG <remadag@gmail.com>
2026-04-02 08:21:58 +0200

    [2026 Krk] update

--- js/scripts.js
+++ js/scripts.js
@@ -226,6 +226,7 @@
             iframe.attr('src', iframe.attr('src'));
         });
         $('.slot').click(function() {
+            if ($(this).hasClass('service-slot')) return;
             window.scrollTo(0, 0);
             location.hash = $(this).attr('id');
         });
